656c0d343349be563aa2f9c8a13b8e9a1a70ab99
mikemacgregor <mike.macgregor@rogers.com>
2021-02-12 13:51:05 -0500

    getenv

--- examples/provider.php
+++ examples/provider.php
@@ -2,16 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 use Hayageek\OAuth2\Client\Provider\Yahoo;
 
 // Replace these with your token settings
 // Create a project at https://developer.yahoo.com/apps
 
-$clientId = $_ENV['YAHOO_CLIENT_ID'];
-$clientSecret = $_ENV['YAHOO_CLIENT_SECRET'];
+$clientId = getenv(['YAHOO_CLIENT_ID']); // $_ENV['YAHOO_CLIENT_ID'];
+$clientSecret = getenv('YAHOO_CLIENT_SECRET'); // $_ENV['YAHOO_CLIENT_SECRET'];
 // Change this if you are not using the built-in PHP server
 $redirectUri = 'http://oauth2-yahoo.herokuapp.com/examples/index.php';
 
